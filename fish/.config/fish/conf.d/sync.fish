189bba3db2dfeb397c397b0952065c54576e2018
ishrq <123639873+ishrq@users.noreply.github.com>
2023-02-21 13:46:27 +0600

    Skip fisher

--- fish/.config/fish/conf.d/sync.fish
+++ fish/.config/fish/conf.d/sync.fish
@@ -22,7 +22,7 @@ function sync -d "Sync files between devices"
     rsync -auzP ~/.config/newsboat/ $dest:~/.config/newsboat/
     rsync -auzP ~/.local/share/newsboat/cache.db $dest:~/.local/share/newsboat/
     rsync -auzP ~/.config/fish/config.fish $dest:~/.config/fish/
-    rsync -auzP ~/.config/fish/conf.d/ $dest:~/.config/fish/conf.d/
+    rsync -auzP --exclude={autopair,fzf,sponge}.fish ~/.config/fish/conf.d/ $dest:~/.config/fish/conf.d/
     rsync -auzP ~/.config/nvim/snippets/ $dest:~/.config/nvim/snippets/
     rsync -auzP ~/.config/nvim/skeletons/ $dest:~/.config/nvim/skeletons/
 
