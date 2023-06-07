fc8580174961ae6c8f5900af93c50780951174ad
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-07 10:46:34 +0600

    Nvim: Modify config for gitsigns

--- nvim/.config/nvim/lua/plugins/git-plug.lua
+++ nvim/.config/nvim/lua/plugins/git-plug.lua
@@ -5,6 +5,46 @@ return {
     cmd = 'Gitsigns',
     config = function()
         require('gitsigns').setup{
+            signs = {
+                add          = { text = '|' },
+                change       = { text = '|' },
+                delete       = { text = '|' },
+                topdelete    = { text = '‾' },
+                changedelete = { text = '~' },
+                untracked    = { text = '┆' },
+            },
+            signcolumn = true,  -- Toggle`:Gitsigns toggle_signs`
+            numhl      = false, -- Toggle`:Gitsigns toggle_numhl`
+            linehl     = false, -- Toggle`:Gitsigns toggle_linehl`
+            word_diff  = false, -- Toggle`:Gitsigns toggle_word_diff`
+            watch_gitdir = {
+                interval = 1000,
+                follow_files = true
+            },
+            attach_to_untracked = true,
+            current_line_blame = false, -- Toggle`:Gitsigns toggle_current_line_blame`
+            current_line_blame_opts = {
+                virt_text = true,
+                virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+                delay = 1000,
+                ignore_whitespace = false,
+            },
+            current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+            sign_priority = 6,
+            update_debounce = 100,
+            status_formatter = nil, -- Use default
+            max_file_length = 40000, -- Disable if file is longer than this (in lines)
+            preview_config = {
+                -- Options passed to nvim_open_win
+                border = 'single',
+                style = 'minimal',
+                relative = 'cursor',
+                row = 0,
+                col = 1
+            },
+            yadm = {
+                enable = false
+            },
             on_attach = function(bufnr)
                 local gs = package.loaded.gitsigns
 
@@ -13,6 +53,7 @@ return {
                     opts.buffer = bufnr
                     vim.keymap.set(mode, l, r, opts)
                 end
+
                 -- Navigation
                 map('n', ']c', function()
                     if vim.wo.diff then return ']c' end
@@ -44,6 +85,6 @@ return {
                 -- Text object
                 map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
             end
-            }
-        end
-    }
+        }
+    end
+}
