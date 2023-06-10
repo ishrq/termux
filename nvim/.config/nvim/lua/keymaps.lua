76fba2928eb50fd4098b30a7fd8062a5bd76e231
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-10 21:27:03 +0600

    Nvim: Minor tweak

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -7,21 +7,21 @@ local map = vim.keymap.set
 local def = { noremap = true, silent = true }
 
 -- General
-map('v', '.', ':normal .<CR>', { desc = "Apply repeat on select"})
-map("n", "<c-a>", ":keepjumps normal! GVgg<cr>", def, { desc = "Select all" })
-map('i', '<c-f>', '<C-g>u<Esc>[S1z=`]a<c-g>u', def, {desc = "Fix spelling"})
-map("n", "<leader>=", ":set spell!<cr>", { desc = "Spelling" })
-map("n", "<leader>8", ':execute "set cc=" . (&cc == "" ? "80" : "")<cr>', def, { desc = "Column color" })
-map("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Substitute word under cursor" })
-map("n", "<leader>x", ":!chmod +x %<CR>", def, { desc = "Make executable" })
+map('v', '.', ':normal .<CR>', {desc="Visual mode dot repeat"})
+map("n", "<c-s-a>", ":keepjumps normal! GVgg<cr>", def, {desc="Select all" })
+map('i', '<c-f>', '<C-g>u<Esc>[S1z=`]a<c-g>u', def, {desc="Fix spelling"})
+map("n", "<leader>=", ":set spell!<cr>", {desc="Spelling"})
+map("n", "<leader>8", ':execute "set cc=" . (&cc == "" ? "80" : "")<cr>', def, {desc="Column color"})
+map("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {desc="Substitute word"})
+map("n", "<leader>x", ":!chmod +x %<CR>", def, { desc ="Make executable"})
 
 -- Better indenting
 map("v", "<", "<gv^")
 map("v", ">", ">gv^")
 
 -- Register
-map("", "gp", "`[v`]", def, { desc = "Select pasted" })
-map("x", "<leader>p", [["_dP]], { desc = "Replace selection with register" })
+map("", "gp", "`[v`]", def, {desc="Select pasted"})
+map("x", "<leader>p", [["_dP]], {desc="Replace selected with register"})
 
 -- Command mode movement
 map("c", "<c-n>", "<down>")
@@ -34,20 +34,24 @@ map("i", "<c-h>", "<left>")
 map("i", "<c-l>", "<right>")
 
 -- Move through line breaks
-map({ "n", "v" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-map({ "n", "v" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+map({ "n", "v" }, "j", "v:count == 0 ? 'gj' : 'j'", {expr=true, silent=true })
+map({ "n", "v" }, "k", "v:count == 0 ? 'gk' : 'k'", {expr=true, silent=true })
 
 -- Buffer
-map("n", "]b", "<cmd>bnext<CR>", def, { desc = "Next buffer" })
-map("n", "[b", "<cmd>bprevious<CR>", def, { desc = "Previous buffer" })
+map("n", "]b", "<cmd>bnext<CR>", def, {desc="Next buffer"})
+map("n", "[b", "<cmd>bprevious<CR>", def, {desc="Previous buffer"})
+
+-- Quickfix
+map("n", "]q", '<cmd>cn<cr>', def, {desc="Next quickfix"})
+map("n", "[q", '<cmd>cp<cr>', def, {desc="Previous quickfix"})
 
 -- Move text up/down
-map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Line down" })
-map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Line up" })
-map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Line down" })
-map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Line up" })
-map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Line down" })
-map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Line up" })
+map("n", "<A-j>", "<cmd>m .+1<cr>==", {desc="Line down"})
+map("n", "<A-k>", "<cmd>m .-2<cr>==", {desc="Line up"})
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", {desc="Line down"})
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", {desc="Line up"})
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", {desc="Line down"})
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", {desc="Line up"})
 
 -- Less cursor movement
 map("", "J", "mzJ`z")
@@ -65,18 +69,12 @@ map("i", ".", ".<c-g>u")
 map("i", ";", ";<c-g>u")
 
 -- Tabs, windows, frequent files
-map("n", ",k", ":tab drop ~/.config/nvim/lua/keymaps.lua<cr>", { desc = "Open keymap.lua" })
-map('n', '[t', '<CMD>tabprevious<CR>', {desc = "Previous tab"})
-map('n', ']t', '<CMD>tabnext<CR>', {desc = "Next tab"})
-map('n', '[T', '<CMD>tabmove -1<CR>', {desc = "Shift to previous tab"})
-map('n', ']T', '<CMD>tabmove +1<CR>', {desc = "Shift to next tab"})
+map("n", ",k", ":tab drop ~/.config/nvim/lua/keymaps.lua<cr>", {desc="Open keymap.lua"})
+map('n', '[t', '<CMD>tabmove -1<CR>', {desc = "Shift to previous tab"})
+map('n', ']t', '<CMD>tabmove +1<CR>', {desc = "Shift to next tab"})
 
 -- Diagnostic keymaps
-map("n", "[d", vim.diagnostic.goto_prev, def)
-map("n", "]d", vim.diagnostic.goto_next, def)
+map("n", "]d", vim.diagnostic.goto_next, def, {desc="Next diagnostic"})
+map("n", "[d", vim.diagnostic.goto_prev, def, {desc="Previous diagnostic"})
 map('n', '<leader>e', vim.diagnostic.open_float)
 map('n', '<leader>q', vim.diagnostic.setloclist, def)
-
--- Quickfix
-map("n", "[q", '<cmd>cprev<cr>', def)
-map("n", "]q", '<cmd>cnext<cr>', def)
--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,23 +1,9 @@
 return {
-    "jesseleite/nvim-noirbuddy",
-    dependencies = {"tjdevries/colorbuddy.nvim", branch = "dev"},
-    config = function()
-        require("noirbuddy").setup {
-            preset = 'minimal',
-            -- preset = 'miami-nights',
-            -- preset = 'kiwi',
-            -- preset = 'crt-amber',
-            -- preset = 'crt-green',
-            -- preset = 'slate',
-            colors = {
-                background = "#000000",
-            },
-            styles = {
-                italic = true,
-                bold = false,
-                underline = false,
-                undercurl = true,
-            },
-        }
-    end
+    {
+        'Everblush/nvim',
+        name = 'everblush',
+        config = function ()
+            vim.cmd.colorscheme("everblush")
+        end
+    },
 }
