c00e329684d1b0c8d6b33c42e7ddebf75b5ee65f
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-10 21:50:05 +0600

    Nvim: Minor tweak

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -3,7 +3,16 @@ return {
         'Everblush/nvim',
         name = 'everblush',
         config = function ()
+            require("everblush").setup{
+                override = {
+                    Normal = {
+                        bg = '#000000',
+                    }
+                }
+            }
             vim.cmd.colorscheme("everblush")
         end
     },
 }
+
+-- this is a comment
