c96e0684ccf2c3485820b1e904720714212a185d
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-13 02:23:01 +0600

    Nvim: Add keymaps to jump to mini.ai text-objects

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -8,10 +8,10 @@ local def = { noremap = true, silent = true }
 
 -- General
 map("v", ".", ":normal .<CR>", {desc="Visual mode dot repeat"})
-map('i', '<c-z>', '<C-g>u<Esc>[S1z=`]a<c-g>u', def, {desc="Fix spelling"})
-map("n", "<leader>=", ":set spell!<cr>", {desc="Spelling"})
-map("n", "<leader>8", ':execute "set cc=" . (&cc == "" ? "80" : "")<cr>', def, {desc="Column color"})
-map("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {desc="Substitute word"})
+map('i', '<c-z>', '<C-g>u<Esc>[S1z=`]a<c-g>u', {desc="Fix spelling"})
+map("n", "<leader>=", ":set spell!<cr>", {desc="Toggle spell check"})
+map("n", "<leader>8", ':execute "set cc=" . (&cc == "" ? "80" : "")<cr>', def, {desc="Toggle character column"})
+map("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {desc="Replace current word"})
 map("n", "<leader>x", ":!chmod +x %<CR>", def, { desc ="Make executable"})
 
 -- Better indenting
@@ -20,7 +20,7 @@ map("v", ">", ">gv^")
 
 -- Register
 map("", "gp", "`[v`]", def, {desc="Select pasted"})
-map("x", "<leader>p", [["_dP]], {desc="Replace selected with register"})
+map("x", "<leader>p", [["_dP]], {desc="Paste on selection"})
 
 -- Command mode movement
 map("c", "<c-n>", "<down>")
@@ -59,3 +59,13 @@ map('n', '<leader>tl', '<CMD>tabmove +1<CR>', {desc = "Shift to next tab"})
 -- Diagnostic keymaps
 map('n', '<leader>e', vim.diagnostic.open_float)
 map('n', '<leader>q', vim.diagnostic.setloclist, def)
+
+-- mini.nvim: mini.ai
+map('n', 'sa', "<cmd>lua MiniAi.move_cursor('left', 'i', 'a')<cr>", def, {desc="Next argument"})
+map('n', 'Sa', "<cmd>lua MiniAi.move_cursor('left', 'i', 'a', {search_method='prev'})<cr>", def, {desc="Previous argument"})
+map('n', 'sd', "<cmd>lua MiniAi.move_cursor('left', 'i', 'd')<cr>", def, {desc="Next date"})
+map('n', 'Sd', "<cmd>lua MiniAi.move_cursor('left', 'i', 'd', {search_method='prev'})<cr>", def, {desc="Previous date"})
+map('n', 'sf', "<cmd>lua MiniAi.move_cursor('left', 'i', 'f')<cr>", def, {desc="Next function"})
+map('n', 'Sf', "<cmd>lua MiniAi.move_cursor('left', 'i', 'f', {search_method='prev'})<cr>", def, {desc="Previous function"})
+map('n', 'su', "<cmd>lua MiniAi.move_cursor('left', 'i', 'u')<cr>", def, {desc="Next url"})
+map('n', 'Su', "<cmd>lua MiniAi.move_cursor('left', 'i', 'u', {search_method='prev'})<cr>", def, {desc="Previous url"})
