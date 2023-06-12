5e16e6e430b70ea5b16fb382e43c97383ddfeeb0
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-12 16:40:01 +0600

    Nvim: Modify kanagawa.nvim defaults

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -18,12 +18,15 @@ return {
                         NormalFloat = { bg = "none" },
                         FloatBorder = { bg = "none" },
                         FloatTitle = { bg = "none" },
-
                         NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-                        -- Popular plugins that open floats will link to NormalFloat by default;
+                        -- Popular plugins that open floats
                         LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
                         MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+                        Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1, blend = vim.o.pumblend },
+                        PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+                        PmenuSbar = { bg = theme.ui.bg_m1 },
+                        PmenuThumb = { bg = theme.ui.bg_p2 },
                     }
                 end,
             }
