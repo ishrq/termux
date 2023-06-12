f89cd20ded41426190dc304b043fb6e72abafa3b
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-12 16:00:47 +0600

    Nvim: Change colorscheme to kanagawa.nvim

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,17 +1,33 @@
 return {
+    --Colorscheme: https://github.com/rebelot/kanagawa.nvim
     {
-        'Everblush/nvim',
-        name = 'everblush',
+        'rebelot/kanagawa.nvim',
+        name = 'kanagawa',
         config = function ()
-            require("everblush").setup{
-                override = {
-                    Normal = {
-                        bg = '#000000',
+            require('kanagawa').setup{
+                dimInactive = true,
+                colors = {
+                    palette = { dragonBlack3 = "#000000" },
+                    theme = {
+                        all = { ui = { bg_gutter = "none", } }
                     }
-                }
-            }
-            vim.cmd.colorscheme("everblush")
-        end
-    },
-}
+                },
+                overrides = function(colors)
+                    local theme = colors.theme
+                    return {
+                        NormalFloat = { bg = "none" },
+                        FloatBorder = { bg = "none" },
+                        FloatTitle = { bg = "none" },
 
+                        NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+                        -- Popular plugins that open floats will link to NormalFloat by default;
+                        LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                        MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                    }
+                end,
+            }
+            require('kanagawa').load('dragon') --wave, lotus, dragon
+        end
+    }
+}
