c8b098e66832a31d35ebe8ad202b6bba16e83f3b
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-13 02:25:15 +0600

    Nvim: Minor tweaks

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -94,20 +94,21 @@ return {
                     vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
                     -- Buffer local mappings.
+                    local map = vim.keymap.set
                     local opts = { buffer = ev.buf }
-                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-                    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-                    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-                    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-                    vim.keymap.set('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
-                    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-                    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-                    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-                    vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, opts)
+                    map('n', 'gD', vim.lsp.buf.declaration, opts)
+                    map('n', 'gd', vim.lsp.buf.definition, opts)
+                    map('n', 'K', vim.lsp.buf.hover, opts)
+                    map('n', 'gi', vim.lsp.buf.implementation, opts)
+                    map('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                    map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+                    map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                    map('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+                    map('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+                    map('n', '<leader>rn', vim.lsp.buf.rename, opts)
+                    map({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+                    map('n', 'gr', vim.lsp.buf.references, opts)
+                    map('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, opts)
                 end,
             })
         end
