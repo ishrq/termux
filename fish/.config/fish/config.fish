10795713b6ff4c047c494c57fed871ad5c46d5b7
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-16 13:19:51 +0600

    Newsboat: Use single config file for all device

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,7 +12,6 @@ if test $USER="ia" && test $hostname="pop-os"
     # Abbreviations
     abbr -a a 'aerc'
     abbr -a b 'vi ~/ARCHIVE/Journal/books.txt'
-    abbr -a n 'newsboat'
     abbr -a bc 'bluetoothctl connect FC:E8:06:E9:03:B9'
     abbr -a bd 'bluetoothctl disconnect FC:E8:06:E9:03:B9'
 
@@ -33,7 +32,6 @@ if test $USER = "u0_a1692" || test $USER = "u0_a215"
 
     # Abbreviations
     abbr -a b 'vi ~/storage/shared/Documents/COMPUTER/ARCHIVE/Journal/books.txt'
-    abbr -a n 'newsboat -C ~/.config/newsboat/config_phone'
 
 end
 
@@ -69,13 +67,11 @@ abbr -a cp 'cp -rv'
 abbr -a mkdir 'mkdir -v'
 abbr -a mv 'mv -v'
 abbr -a y '  yt-dlp'
-#exa
 abbr -a la 'exa -a --icons --group-directories-first'
 abbr -a ld 'exa -d */'
 abbr -a ll 'exa -lhaGFmU --git --icons --group-directories-first'
 abbr -a ls 'exa --icons --group-directories-first'
 abbr -a tree 'exa --icons --tree --level=2'
-#git
 abbr -a g 'git'
 abbr -a ga 'git add'
 abbr -a gc 'git commit'
@@ -83,6 +79,9 @@ abbr -a gd 'git diff'
 abbr -a gk 'git checkout (git branch -a | fzf | xargs)'
 abbr -a gm 'git merge'
 abbr -a gp 'git push'
+abbr -a n 'newsboat'
+abbr -a na 'newsboat -u ~/.config/newsboat/urls-alt'
+abbr -a nv 'newsboat -u ~/.config/newsboat/urls-video'
 abbr -a p 'gopass'
 abbr -a pc 'gopass -c'
 abbr -a po 'gopass otp'
