78c54fa757284537160602e7f4645de7bcccbd10
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-17 15:03:30 +0600

    Nvim: Cleanup

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -3,60 +3,73 @@ local autocmd = vim.api.nvim_create_autocmd
 
 augroup('bufcheck', {clear=true})
 
---Reload config file on change
+-- Reload config file on change
 autocmd('BufWritePost', {
-  group   = 'bufcheck',
-  pattern = vim.env.MYVIMRC,
-  command = 'silent source %'})
+    group   = 'bufcheck',
+    pattern = vim.env.MYVIMRC,
+    command = 'silent source %'
+})
 
---Highlight yanks
+-- Highlight yank
 autocmd('TextYankPost', {
   group    = 'bufcheck',
   pattern  = '*',
-  callback = function() vim.highlight.on_yank{timeout=300} end })
+  callback = function()
+      vim.highlight.on_yank{timeout=300}
+  end
+})
 
---Resume previous position
+-- Resume previous position
 autocmd('BufReadPost', {
   pattern = '*',
-  command = [[ if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif ]], })
+  command = [[ if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif ]],
+})
 
--- Auro resize window
+-- Auto resize splits if window got resized
 autocmd('VimResized', {
   callback = function() vim.cmd("tabdo wincmd =") end,
 })
 
---Remove trailing whitespace on write
+-- Remove trailing whitespace on write
 autocmd('BufWritePre', {
   pattern = '*',
-  command = ":%s/\\s\\+$//e" })
+  command = ":%s/\\s\\+$//e"
+})
 
---Don't auto comment new lines
+-- Don't auto comment new lines
 autocmd('BufEnter', {
   pattern = '*',
-  command = 'set fo-=c fo-=r fo-=o' })
+  command = 'set fo-=c fo-=r fo-=o'
+})
 
---Start git messages in insert mode
+-- Start git messages in insert mode
 autocmd('FileType', {
   group   = 'bufcheck',
   pattern = { 'gitcommit', 'gitrebase', },
-  command = 'startinsert | 1' } )
+  command = 'startinsert | 1'
+})
 
---No backup, swapfile, undofile for gopass
+-- No backup, swapfile, undofile for gopass
 autocmd({'BufRead', 'BufNewFile'}, {
   pattern = { '/dev/shm/gopass*' },
-  command = ' setlocal noswapfile nobackup noundofile shada="" ' } )
+  command = ' setlocal noswapfile nobackup noundofile shada="" '
+})
 
---Color column indicator for 80 characters
+-- Color column indicator for 80 characters
 autocmd({'BufRead', 'BufNewFile'}, {
   pattern = { 'gitcommit', 'markdown', 'text' },
-  command = 'cc=80' } )
+  callback = function()
+    vim.opt_local.cc = '80'
+  end,
+})
 
---Disable color column
+-- Disable color column
 autocmd('BufRead', {
   pattern = {'*/Journal/*', '*/Finance/*', '*/House/*', '*/List/*'},
-  command = 'setlocal cc=' })
+  command = 'setlocal cc='
+})
 
---Markdown/gitcommit
+-- Markdown/gitcommit
 autocmd('FileType', {
     pattern = { "gitcommit", "markdown", "text", "log" },
     callback = function()
@@ -70,7 +83,17 @@ autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {
   group = augroup('TS_FOLD_WORKAROUND', {}),
   callback = function()
     vim.opt.foldmethod = 'expr'
-    vim.opt.foldexpr   = 'nvim_treesitter#foldexpr()' end })
+    vim.opt.foldexpr   = 'nvim_treesitter#foldexpr()'
+end
+})
+
+--HTML/CSS Shiftwidth
+autocmd('FileType', {
+    pattern = { "html", "css" },
+    callback = function()
+        vim.bo.shiftwidth = 2
+    end,
+})
 
 --Create directory on save
 autocmd('BufWritePre', {
@@ -101,14 +124,16 @@ autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave' }, {
     callback = function()
         vim.wo.relativenumber = true
     end,
-    group = numbertogglegroup, })
+    group = numbertogglegroup,
+})
 
 autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter' }, {
     pattern = '*',
     callback = function()
         vim.wo.relativenumber = false
     end,
-    group = numbertogglegroup, })
+    group = numbertogglegroup,
+})
 
 
 --SKELETON
@@ -116,14 +141,17 @@ autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter' }, {
 --Ledger
 autocmd('BufNewFile', {
     pattern = '*/Finance/Ledger/ledger*.txt',
-    command = '0r ~/.config/nvim/skeletons/ledger.txt' })
+    command = '0r ~/.config/nvim/skeletons/ledger.txt'
+})
 
 --HTML
 autocmd('BufNewFile', {
     pattern = '*.html',
-    command = '0r ~/.config/nvim/skeletons/html-skeleton.html' })
+    command = '0r ~/.config/nvim/skeletons/html-skeleton.html'
+})
 
 --CSS
 autocmd('BufNewFile', {
     pattern = {'style.css', 'reset.css'},
-    command = '0r ~/.config/nvim/skeletons/css-reset.css' })
+    command = '0r ~/.config/nvim/skeletons/css-reset.css'
+})
