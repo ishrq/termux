9a8a2adab976d30b18cc12c470cd8204c9c668ef
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-23 20:54:15 +0600

    Fish: Improve sync.fish
    Make directory if needed
    Use log file instead of verbose stdout
    Modify file list
    Use colored echo
    Fix typo

--- fish/.config/fish/conf.d/sync.fish
+++ fish/.config/fish/conf.d/sync.fish
@@ -17,34 +17,39 @@ function sync -d "Sync files between devices"
       set target_ip "192.168.0.103"
     end
 
+    set log ~/storage/emulated/0/Documents/COMPUTER/ARCHIVE/Logs/sync.log
+
     # phone -> phone
     if ping -c 1 $target_ip | grep -q "Unreachable"
-      echo "$target is offline"
+      set_color red; echo "$target is offline"
     else
-      rsync -auzP --exclude=TBR/Audiobook ~/storage/shared/Documents/ $target:~/storage/shared/Documents/
-      rsync -auzP ~/.local/share/newsboat/ $target:~/.local/share/newsboat/
-      rsync -auzP --exclude=config ~/.config/newsboat/ $target:~/.config/newsboat/
+      rsync -ahmuqz --log-file=$log --mkpath --partial --exclude=TBR/Audiobook ~/storage/shared/Documents/ $target:~/storage/shared/Documents/
+      rsync -ahmuqz --log-file=$log --mkpath --partial ~/.local/share/newsboat/ $target:~/.local/share/newsboat/
+      rsync -ahmuqz --log-file=$log --mkpath --partial --exclude=config ~/.config/newsboat/ $target:~/.config/newsboat/
+      set_color green; echo "Sync with $target successful"
     end
 
     # phone -> laptop
     if ping -c 1 192.168.0.104 | grep -q "Unreachable"
-      echo "Laptop is offline"
+      set_color red; echo "Laptop is offline"
     else
-      rsync -auzp ~/storage/shared/documents/computer/ laptop:~/
-      rsync -auzp ~/storage/dcim/screenshots/ laptop:~/archive/screenshots/
-      rsync -auzp ~/storage/shared/backups/ laptop:~/archive/Phone-Backups/Backup-$device/
-      rsync -auzp ~/.local/share/newsboat/ laptop:~/.local/share/newsboat/
-      rsync -auzp --exclude=config ~/.config/newsboat/ laptop:~/.config/newsboat/
-      rsync -auzp ~/.config/fish/config.fish laptop:~/.config/fish/
-      rsync -auzp --exclude={autopair,fzf,sponge}.fish ~/.config/fish/conf.d/ laptop:~/.config/fish/conf.d/
-      rsync -auzp ~/.config/nvim/snippets/ laptop:~/.config/nvim/snippets/
-      rsync -auzp ~/.config/nvim/skeletons/ laptop:~/.config/nvim/skeletons/
-      rsync -auzp --exclude=config.local ~/.config/git/config laptop:~/.config/git/
+      rsync -ahmuqz --log-file=$log --mkpath --partial ~/storage/shared/documents/computer/ laptop:~/
+      rsync -ahmuqz --log-file=$log --mkpath --partial ~/storage/dcim/screenshots/ laptop:~/Archive/screenshots/
+      rsync -ahmuqz --log-file=$log --mkpath --partial ~/storage/shared/backups/ laptop:~/Archive/Phone-Backups/Backup-$device/
+      rsync -ahmuqz --log-file=$log --mkpath --partial ~/.local/share/newsboat/ laptop:~/.local/share/newsboat/
+      rsync -ahmuqz --log-file=$log --mkpath --partial --exclude=config ~/.config/newsboat/ laptop:~/.config/newsboat/
+      rsync -ahmuqz --log-file=$log --mkpath --partial ~/.config/fish/config.fish laptop:~/.config/fish/
+      rsync -ahmuqz --log-file=$log --mkpath --partial --exclude={autopair,fzf,sponge}.fish ~/.config/fish/conf.d/ laptop:~/.config/fish/conf.d/
+      rsync -ahmuqz --log-file=$log --mkpath --partial ~/.config/nvim/snippets/ laptop:~/.config/nvim/snippets/
+      rsync -ahmuqz --log-file=$log --mkpath --partial ~/.config/nvim/skeletons/ laptop:~/.config/nvim/skeletons/
+      rsync -ahmuqz --log-file=$log --mkpath --partial --exclude=config.local ~/.config/git/config laptop:~/.config/git/
+      set_color green; echo "Sync with laptop successful"
     end
 
 
-    # laptop
+# laptop
   else
+    set log ~/ARCHIVE/Logs/sync.log
     for target in s8 m10
       if test $target = "s8"
         set target_ip "192.168.0.103"
@@ -55,17 +60,18 @@ function sync -d "Sync files between devices"
       end
 
       if ping -c 1 $target_ip | grep -q "Unreachable"
-        echo "$target is offline"
+        set_color red; echo "$target is offline"
       else
-        rsync -auzP ~/RESOURCES/ --exclude=Fonts/ $target:~/storage/shared/Documents/COMPUTER/RESOURCES/
-        rsync -auzP ~/ARCHIVE/{Workouts,Daybook,Ledger,Weight-Log,Logs,Journal} $target:~/storage/shared/Documents/COMPUTER/ARCHIVE/
-        rsync -auzp ~/.local/share/newsboat/ $target:~/.local/share/newsboat/
-        rsync -auzp --exclude=config ~/.config/newsboat/ $target:~/.config/newsboat/
-        rsync -auzp ~/.config/fish/config.fish $target:~/.config/fish/
-        rsync -auzp --exclude={autopair,fzf,sponge}.fish ~/.config/fish/conf.d/ $target:~/.config/fish/conf.d/
-        rsync -auzp ~/.config/nvim/snippets/ $target:~/.config/nvim/snippets/
-        rsync -auzp ~/.config/nvim/skeletons/ $target:~/.config/nvim/skeletons/
-        rsync -auzp --exclude=config.local ~/.config/git/config $target:~/.config/git/
+        rsync -ahmuqz --log-file=$log --mkpath --partial --exclude={Fonts,Fork-bombs,gitignore} ~/RESOURCES/ $target:~/storage/shared/Documents/COMPUTER/RESOURCES/
+        rsync -ahmuqz --log-file=$log --mkpath --partial --exclude={Audiobook,Ebook,Old_References,Papers,Phone-Backups,Projects,Screenshots,Travel} ~/ARCHIVE/ $target:~/storage/shared/Documents/COMPUTER/ARCHIVE/
+        rsync -ahmuqz --log-file=$log --mkpath --partial ~/.local/share/newsboat/ $target:~/.local/share/newsboat/
+        rsync -ahmuqz --log-file=$log --mkpath --partial --exclude=config ~/.config/newsboat/ $target:~/.config/newsboat/
+        rsync -ahmuqz --log-file=$log --mkpath --partial ~/.config/fish/config.fish $target:~/.config/fish/
+        rsync -ahmuqz --log-file=$log --mkpath --partial --exclude={autopair,fzf,sponge}.fish ~/.config/fish/conf.d/ $target:~/.config/fish/conf.d/
+        rsync -ahmuqz --log-file=$log --mkpath --partial ~/.config/nvim/snippets/ $target:~/.config/nvim/snippets/
+        rsync -ahmuqz --log-file=$log --mkpath --partial ~/.config/nvim/skeletons/ $target:~/.config/nvim/skeletons/
+        rsync -ahmuqz --log-file=$log --mkpath --partial --exclude=config.local ~/.config/git/config $target:~/.config/git/
+        set_color green; echo "Sync with $target successful"
       end
     end
 
