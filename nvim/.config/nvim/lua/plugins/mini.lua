7a3037d1dd1b8c576f416205f34002be4765fb45
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-23 23:54:48 +0600

    Nvim: Modify keymaps for mini.files

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -68,7 +68,20 @@ return {
         yank       = { suffix = '', options = {} },
       }
 
-      require('mini.files').setup()
+      require('mini.files').setup{
+        mappings = {
+          close       = 'q',
+          go_in       = '<Right>',
+          go_in_plus  = '<CR>',
+          go_out      = '<Left>',
+          go_out_plus = '',
+          reset       = '<BS>',
+          show_help   = 'g?',
+          synchronize = '=',
+          trim_left   = '<',
+          trim_right  = '>',
+        },
+      }
 
       -- Show/hide dot files in file tree
       local show_dotfiles = true
@@ -102,6 +115,31 @@ return {
           vim.keymap.set('n', 'g~', files_set_cwd, { buffer = args.data.buf_id })
         end,
       })
+
+      -- Open in split
+      local map_split = function(buf_id, lhs, direction)
+        local rhs = function()
+          -- Make new window and set it as target
+          local new_target_window
+          vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
+            vim.cmd(direction .. ' split')
+            new_target_window = vim.api.nvim_get_current_win()
+          end)
+          MiniFiles.set_target_window(new_target_window)
+        end
+        -- Adding `desc` will result into `show_help` entries
+        local desc = 'Split ' .. direction
+        vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+      end
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'MiniFilesBufferCreate',
+        callback = function(args)
+          local buf_id = args.data.buf_id
+          map_split(buf_id, 'gs', 'belowright horizontal')
+          map_split(buf_id, 'gv', 'belowright vertical')
+        end,
+      })
+
 
       require('mini.hipatterns').setup{
         highlighters = {
