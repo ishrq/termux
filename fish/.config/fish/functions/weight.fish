7a416fa4fdc2fac9e26186d894f6f89e907fdf99
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-24 16:12:12 +0600

    Fish: Change file format for weight.fish script

--- fish/.config/fish/functions/weight.fish
+++ fish/.config/fish/functions/weight.fish
@@ -2,6 +2,6 @@
 # ---
 
 function weight -d "Weight journal"
-  set file ~/storage/shared/Documents/COMPUTER/ARCHIVE/Weight-log/weight-$(date +"%Y").txt
+  set file ~/storage/shared/Documents/COMPUTER/ARCHIVE/Weight-log/weight-$(date +"%Y").log
   echo $(date +"%F  $argv") >> $file
 end
