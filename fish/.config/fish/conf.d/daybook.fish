49a0da8599ad73a7b9fa422586ad6e173ee57700
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-24 14:13:46 +0600

    Fish: change working directory of daybook and workout scripts

--- fish/.config/fish/conf.d/daybook.fish
+++ fish/.config/fish/conf.d/daybook.fish
@@ -6,12 +6,14 @@ function daybook -d "Work Daybook"
     set day $(date +"%F")
 
     if test -d "$HOME/ARCHIVE"
+        set dir ~/ARCHIVE/Daybook/
         set file ~/ARCHIVE/Daybook/Daybook_$day.md
     else
+        set dir ~/storage/shared/Documents/COMPUTER/ARCHIVE/Daybook/
         set file ~/storage/shared/Documents/COMPUTER/ARCHIVE/Daybook/Daybook_$day.md
     end
 
-    $EDITOR $file
+    $EDITOR -c ":cd $dir" $file
 end
 
 abbr -a d 'daybook'
--- fish/.config/fish/functions/workout.fish
+++ fish/.config/fish/functions/workout.fish
@@ -3,11 +3,12 @@
 
 
 function workout -d "Workout log"
+  set dir ~/storage/shared/Documents/COMPUTER/ARCHIVE/Workouts/
   set file ~/storage/shared/Documents/COMPUTER/ARCHIVE/Workouts/workout-$(date +"%Y-%m").txt
 
   switch $argv
     case $argv
       echo "$(date +"%n%F $argv")" >> $file
   end
-  $EDITOR $file
+  $EDITOR -c ":cd $dir" $file
 end
