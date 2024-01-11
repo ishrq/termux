6de7c4b2e6fb199cbf0d7962b97ea48e299246cf
ishrq <123639873+ishrq@users.noreply.github.com>
2024-01-11 17:36:31 +0600

    From PC

---
+++ fish/.config/fish/conf.d/backup.fish
@@ -0,0 +1,18 @@
+# Author: IA
+# ---
+
+function backup -d "Backup files"
+
+  set log ~/ARCHIVE/Logs/backup.log # log file
+  set target /media/sdcard-bkp1/Backups/ # backup directory
+
+  # set date $(date +"%Y%d%m%H%M%S")
+  # tar -cvzf $target/private-files-$date.tar.gz ~/ARCHIVE/{Client-Invoices,Finances,Journals,Logs,Phone-Backups,Receipts,References,Resume,Screenshots,Travel}
+  # tar -rvzf $target/private-files-$date.tar.gz ~/RESOURCES
+  # echo "$date" >> $log
+
+  rsync -ahmuqz --log-file=$log --mkpath --partial ~/ARCHIVE/{Client-Invoices,Finances,Journals,Logs,Phone-Backups,Receipts,References,Resume,Screenshots,Travel} $target/ARCHIVE/
+  rsync -ahmuqz --log-file=$log --mkpath --partial ~/RESOURCES $target
+
+  set_color green; echo "Backup successful!"
+end
