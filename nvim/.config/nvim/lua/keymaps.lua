2878d63f9be08047e231baa2106910b441fb8eec
ishrq <123639873+ishrq@users.noreply.github.com>
2024-03-16 16:39:02 +0600

    Add keymap

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -56,13 +56,11 @@ map('n', '<leader>q', vim.diagnostic.setloclist, default)
 
 -- Smart `dd` (don't yank blank lines)
 -- https://nanotipsforvim.prose.sh/keeping-your-register-clean-from-dd
-map('n', 'dd', function ()
+map('n', 'dd', function() if vim.fn.getline(".") == "" then return '"_dd' end return 'dd' end, {expr = true})
 
-  if vim.fn.getline(".") == "" then return '"_dd' end
-
-  return 'dd'
-
-end, {expr = true})
+-- .repeat & macro on visually selected
+map("x", ".", ":norm .<CR>", nosilent)
+map("x", "@", ":norm @q<CR>", nosilent)
 
 
 -- mini.basic
